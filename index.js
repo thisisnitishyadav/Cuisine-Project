b996d1edb7ccb9b224b5b5e480d280fb5d468beb
Abhishek Cheekati <101444948+Abhishekch756@users.noreply.github.com>
2024-06-17 22:06:15 +0530

    Update index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+// server/server.js
+const express = require('express');
+const connectDB = require('./config/db');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/auth');
+require('dotenv').config();
+
+const app = express();
+connectDB();
+
+app.use(bodyParser.json());
+
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
